d13613bb92ef733f0107a04e458f0059c45c724f
Jascha030 <contact@jaschavanaalst.nl>
2022-03-24 23:39:32 +0100

    ✨ Cli struct with derive, and parse arguments.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Parser)]
+#[clap(name = "Teleport Dir")]
+#[clap(author = "Jascha030 <contact@jaschavanaalst.nl>")]
+#[clap(version = "1.0")]
+#[clap(about = "Navigate into the origin dir of a symlinked file", long_about = None)]
+struct Cli {
+    #[clap(parse(from_os_str))]
+    path: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    println!("Your argument reads: {:?}", cli.path);
 }
